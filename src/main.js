0695dc3950ef6032a954808dd9b87c610f9f018a
v_gqgqgao <v_gqgqgao@tencent.com>
2022-05-31 15:29:39 +0800

    test: 测试webhooks

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 const app = require('./app')
 const config = require('./app/config')
-console.log('test')
 app.listen(config.port, '0.0.0.0', () => {
   console.log(`服务器启动成功，端口号为：${config.port}`)
 })
